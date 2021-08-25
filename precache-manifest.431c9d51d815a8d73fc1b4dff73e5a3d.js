9073b5a776362db94be57b7144b9de2a252d3598
Christoph Klemenjak <christoph.klemenjak@gmail.com>
2021-08-25 20:30:50 +0200

    Updates

---
+++ precache-manifest.431c9d51d815a8d73fc1b4dff73e5a3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92e8e9211f6c768082c49b7e5859674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d189557af54021767d",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "7b641d3aef37ee5168c6",
+    "url": "/static/js/2.9dcf6a68.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.9dcf6a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d189557af54021767d",
+    "url": "/static/js/main.aaceeb31.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "d597a9ca79016c81b655fe2058a94bcd",
+    "url": "/static/media/klemenjak.d597a9ca.jpg"
+  },
+  {
+    "revision": "8838064134f8d4ed5be2491fb231f2f3",
+    "url": "/static/media/resume.88380641.pdf"
+  }
+]);
